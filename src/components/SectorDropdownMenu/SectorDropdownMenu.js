d93551ed9568d8b365696739fea2f9a6b79dd24f
mjaumi <mjaumi2864@gmail.com>
2023-02-03 17:16:40 +0600

    added a loading icon in the menu

--- src/components/SectorDropdownMenu/SectorDropdownMenu.js
+++ src/components/SectorDropdownMenu/SectorDropdownMenu.js
@@ -10,26 +10,33 @@ const SectorDropdownMenu = ({ dropdownMenu, dropdownPopoverShow, dropdownValue, 
             }
         >
             {
-                dropdownMenu.map(item => <div
-                    key={item._id}
-                    className='mx-4 mb-4'>
-                    <p className='font-semibold my-4'>{item.title}</p>
-                    {
-                        item.categories.map(sector => <button
-                            type='button'
-                            key={sector}
-                            className={
-                                `${dropdownPopoverShow ? 'scale-y-100' : 'scale-y-0'} ${dropdownValue === sector ? 'bg-primary text-base-100' : 'bg-base-100 text-primary'} text-sm py-2 px-4 font-normal w-full whitespace-nowrap hover:bg-primary hover:text-base-100 duration-300 origin-top flex justify-start rounded-lg my-1`
-                            }
-                            onClick={e => {
-                                setDropdownValue(sector);
-                                setDropdownPopoverShow(!dropdownPopoverShow);
-                            }}
-                        >
-                            {sector}
-                        </button>)
-                    }
-                </div>)
+                dropdownMenu ?
+
+                    dropdownMenu.map(item => <div
+                        key={item._id}
+                        className='mx-4 mb-4'>
+                        <p className='font-semibold my-4'>{item.title}</p>
+                        {
+                            item.categories.map(sector => <button
+                                type='button'
+                                key={sector}
+                                className={
+                                    `${dropdownPopoverShow ? 'scale-y-100' : 'scale-y-0'} ${dropdownValue === sector ? 'bg-primary text-base-100' : 'bg-base-100 text-primary'} text-sm py-2 px-4 font-normal w-full whitespace-nowrap hover:bg-primary hover:text-base-100 duration-300 origin-top flex justify-start rounded-lg my-1`
+                                }
+                                onClick={e => {
+                                    setDropdownValue(sector);
+                                    setDropdownPopoverShow(!dropdownPopoverShow);
+                                }}
+                            >
+                                {sector}
+                            </button>)
+                        }
+                    </div>)
+
+                    :
+                    <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+                        <button className='btn btn-square bg-transparent border-0 h-20 w-20 loading'></button>
+                    </div>
             }
         </div>
     );
